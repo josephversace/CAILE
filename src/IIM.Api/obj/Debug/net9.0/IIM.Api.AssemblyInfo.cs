1b1a30ecd403a3ddca2890596b0e0f9bdb4e78b9
josephversace <josephversace@gmail.com>
2025-08-14 13:49:50 -0400

    Fixed Api bugs

--- src/IIM.Api/obj/Debug/net9.0/IIM.Api.AssemblyInfo.cs
+++ src/IIM.Api/obj/Debug/net9.0/IIM.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IIM.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fff0848165be120b18c45a3983313a0300a93d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6caf1e49d16dfba70abf2aca32007105f2dd7ac1")]
 [assembly: System.Reflection.AssemblyProductAttribute("IIM.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IIM.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
